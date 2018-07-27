87f8712f06508df5791fe4fb4878ec4435737afb
Ian Campbell <iancampbell@Ians-MacBook-Pro.local>
2018-07-27 15:07:11 -0400

    updates

--- includes/facilities.inc.php
+++ includes/facilities.inc.php
@@ -3,20 +3,23 @@
 $arr[] = ['Brooklyn Campus Library'];/// -----
 $arr[] = ['Activity Resource Center'];/// -----
 
-$arr[] = ['Computer Labs'];///
-$arr[] = ['Digital Output Center'];/// Engineering Building, 2nd Floor -----
-$arr[] = ['3D Printing Center'];/// Engineering Building, 2nd Floor, Room 211 -----
-$arr[] = ['Engineering Computer Lab (EDS)'];/// Engineering Building, 2nd Floor -----
-$arr[] = ['Foundation Media Lab'];/// Main Building, 3rd Floor 36?
-$arr[] = ['Higgins Hall Computer Labs'];/// Higgins Hall North, 2nd and 3rd Floors
-$arr[] = ['Machinery Computer Lab'];/// Machinery Building, 1st Floor
-$arr[] = ['Learning Resource Center'];/// DeKalb Hall, 4th Floor
+$arr[] = ['Computer Labs -- Higgins Hall '];/// Higgins Hall North, 2nd and 3rd Floors
+$arr[] = ['Computer Labs -- Machinery '];/// Machinery Building, 1st Floor
 
-$arr[] = ['Rapid Prototyping Room'];///
-$arr[] = ['Photo Studio'];/// main 3rd 34
-$arr[] = ['Plaster Shop'];/// main 3rd 34
-$arr[] = ['Wood Shop'];/// main 3rd 33
+$arr[] = ['Digital Output Center'];/// Engineering Building, 2nd Floor -----
+$arr[] = ['3D Printing Center'];/// Engineering Building, 2nd Floor, Room 211 ----- Steuben Room 315
+$arr[] = ['Engineering Computer Lab (EDS)'];/// Engineering Building, 2nd Floor -----
+$arr[] = ['Foundation Media Lab'];/// Main Building, 3rd Floor 36? -----
+$arr[] = ['Language Resource Center'];/// DeKalb Hall, 4th Floor -----
+
+$arr[] = ['Industrial Design Shop'];///
+$arr[] = ['Form and Tech Lab'];///
+$arr[] = ['Rapid Prototyping Room'];/// ps 4th floor -----
+$arr[] = ['Photo Studio'];/// Steuben Hall Room 305 -----
+$arr[] = ['CNC Lab'];/// Engineering LL -----
+$arr[] = ['Plaster Shop'];///
+$arr[] = ['Wood Shop'];/// Pratt Studios 506 -----
 $arr[] = ['Metal Shop'];///
-$arr[] = ['Laser Room'];///
+$arr[] = ['Laser Cutting'];/// Steuben 315
 
 ?>
